19053f29b5a1caef5f29d7bd9d3483c6115bfaac
Jefferson Yepez <jeffersonyepez621@gmail.com>
2025-07-15 21:35:55 -0500

    CV

--- src/Components/Hero/HeroButtons.jsx
+++ src/Components/Hero/HeroButtons.jsx
@@ -12,7 +12,15 @@ const HeroButtons = () => {
     return (
         <div className="container-buttons">
             <button onClick={scrollToContact}><FontAwesomeIcon icon={faEnvelope} />Contact me</button>
-            <button ><FontAwesomeIcon icon={faArrowDown} />Download CV</button>
+            <button onClick={() => {
+                const link = document.createElement('a');
+                link.href = '/Jefferson_Yepez_CV.pdf';
+                link.download = 'Jefferson_Yepez_CV.pdf';
+                link.click();
+            }}>
+                <FontAwesomeIcon icon={faArrowDown} /> Download CV
+            </button>
+
         </div>
     )
 };
