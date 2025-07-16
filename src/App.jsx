4e107f7d8503bd4d975ee273320ad0fd5a6a5ef0
Jefferson Yepez <jeffersonyepez621@gmail.com>
2025-07-15 21:09:16 -0500

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import './../public/styles/styles.css'
-import Nav from './Components/Layout/navBar/Nav.jsx'
-import Hero from './Components/hero/Hero.jsx'
-import AboutMe from './Components/aboutMe/AboutMe.jsx'
-import Skills from './Components/skills/Skills.jsx'
-import Projects from './Components/projects/Projects.jsx'
-import Contact from './Components/contact/Contact.jsx'
-import Footer from './Components/Layout/footer/Footer.jsx'
+import Nav from './Components/Layout/navBar/Nav'
+import Hero from './Components/hero/Hero'
+import AboutMe from './Components/aboutMe/AboutMe'
+import Skills from './Components/skills/Skills'
+import Projects from './Components/projects/Projects'
+import Contact from './Components/contact/Contact'
+import Footer from './Components/Layout/footer/Footer'
 
 function App() {
   return (
